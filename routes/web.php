566d84e5fa30d6d9903ed74e2dcb3a14d8c498a9
Dat Tran Le Viet <dattlv@deha-soft.com>
2024-01-03 09:48:39 +0000

    Revert "Merge branch 'feature/create_layouts' into 'develop'"
    This reverts merge request !3

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.index');
-});
-
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-
-Route::get('/register', function () {
-    return view('auth.register');
+    return view('welcome');
 });
