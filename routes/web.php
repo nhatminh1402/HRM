9ced75ecd5bcc662f41600c1365b4f6ff3c96340
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-04 18:26:21 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -16,82 +16,83 @@ Route::get('/', function () {
     return view('admin.positions.index');
 });
 
-Route::prefix('user')->group(function () {
+Route::prefix('user')->name("user.")->group(function () {
     // 1. Router cho trang Home
-    Route::get("HomePage", function () {
+    Route::get("home-page", function () {
         return view("user.pages.index");
-    });
-    //2. Router cho trang xem thông tin chi tiết nhân viên
+    })->name('home');
+    // 2. Router cho trang xem thông tin chi tiết nhân viên
     Route::get("employee-infor", function () {
         return view("user.pages.employee_infor");
-    });
-    //3. Router cho trang xem các phòng ban
-    Route::get("Departments", function () {
+    })->name('employee-info');
+    // 3. Router cho trang xem các phòng ban
+    Route::get("departments", function () {
         return view("user.pages.manage_department");
-    });
-    //4. Router cho trang xem danh sách nhân viên của phòng ban
-    Route::get("DepartmentDetail", function () {
+    })->name('departments');
+    // 4. Router cho trang xem danh sách nhân viên của phòng ban
+    Route::get("department-detail", function () {
         return view("user.pages.department_detail");
-    });
-    //5. Router cho trang xem bảng lương cá nhân
-    Route::get("Salary", function () {
+    })->name('department-detail');
+    // 5. Router cho trang xem bảng lương cá nhân
+    Route::get("salary", function () {
         return view("user.pages.manage_salary");
-    });
-    //6. Router cho trang xem danh sách công tác
+    })->name('salary');
+    // 6. Router cho trang xem danh sách công tác
     Route::get("schedule", function () {
         return view("user.pages.manage_business_travel");
-    });
+    })->name('schedule');
+    
     //7. Router cho trang xem danh sách khen thưởng
     Route::get("reward", function () {
         return view("user.pages.list_reward");
-    });
+    })->name("reward");
 
     //8. Router cho trang xem lịch sử bị kỷ luật
     Route::get("punishments", function () {
         return view("user.pages.list_punishment");
-    });
+    })->name("punishments");
 
     //9. Router cho trang xem thông tin tài khoản
     Route::get("infors", function () {
         return view("user.pages.user_infors");
-    });
+    })->name("infors");
 
     //10. Router cho trang đổi mật khẩu
     Route::get("change-password", function () {
         return view("user.pages.change_password");
-    });
+    })->name("change-password");
 });
 
 Route::get('/admin/position', function () {
     return view('admin.positions.index');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::prefix('/position')->group(function () {
-        // Danh sách + thêm mới chwcs vụ
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::prefix('/position')->name('position.')->group(function () {
+        // Danh sách + thêm mới chức vụ
         Route::get('/home', function () {
             return view('admin.pages.positions.index');
-        });
-        //Sửa chức vụ
+        })->name('home');
+        // Sửa chức vụ
         Route::get('/edit-position', function () {
             return view('admin.pages.positions.edit_position');
-        });
+        })->name('edit-position');
         // Danh sách + thêm mới chuyên môn
         Route::get('/major', function () {
             return view('admin.pages.positions.major');
-        });
+        })->name('major');
         // Sửa chuyên môn
         Route::get('/edit-major', function () {
             return view('admin.pages.positions.edit_major');
-        });
-        // thêm mới nhân viên
+        })->name('edit-major');
+        // Thêm mới nhân viên
         Route::get('/create-employee', function () {
             return view('admin.pages.positions.create_employee');
-        });
-        // danh sách nhân viên
+        })->name('create-employee');
+        // Danh sách nhân viên
         Route::get('/list-employee', function () {
             return view('admin.pages.positions.list_employee');
-        });
+        })->name('list-employee');
     });
 });
 
