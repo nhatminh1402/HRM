04121146fac1edef7578d69e85b7377bde5e2b92
Phước Nguyễn <phuocnguyen.081096@gmail.com>
2024-01-06 23:28:58 +0700

    create layouts manager list creat update change password for account

--- routes/admin/manage_user.php
+++ routes/admin/manage_user.php
@@ -10,4 +10,17 @@ Route::prefix('/account')->name('account.')->group(function () {
     Route::get('/personal', function () {
         return view('admin.pages.user.personal_information');
     })->name('personal');
+    Route::get('/create', function () {
+        return view('admin.pages.user.create_account');
+    })->name('create');
+    Route::get('/edit', function () {
+        return view('admin.pages.user.edit_account');
+    })->name('edit');
+    Route::get('/list', function () {
+        return view('admin.pages.user.list_account');
+    })->name('list');
+    Route::get('/change-password', function () {
+        return view('admin.pages.user.change_password');
+    })->name('change-password');
 });
+
