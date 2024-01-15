c919c3c82a0ed4d6fb681d828e9babfa472a9215
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-15 14:15:39 +0700

    Done UI Maange Reward

--- resources/views/admin/pages/reward/ManageReward.blade.php
+++ resources/views/admin/pages/reward/ManageReward.blade.php
@@ -1,42 +1,116 @@
 @extends('admin.layouts.app')
-@section('title', 'QUẢN LÝ KHEN THƯỞNG')
+@section('title', 'THÊM LOẠI KHEN THƯỞNG')
+@section('css')
+    <style>
+        .card-header:hover {
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
+        }
+
+        .card-header {
+            transition: all ease-out 0.1s;
+        }
+    </style>
+@endsection
 @section('content')
     <div class="row">
         <div class="col-md-12">
             <div class="bg-white rounded shadow p-3 mb-4 mt-4">
-                <div class="row border-bottom">
+                <div class="row">
                     <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
-                        <p style="font-size: 22px; font-weight: bold" class="mb-0 text-center  text-lg-start">QUẢN LÝ KHEN
-                            THƯỞNG
+                        <p style="font-size: 22px; font-weight: bold" class="mb-0 text-center  text-lg-start">QUẢN LÝ LOẠI
+                            KHEN THƯỞNG
                         </p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="c">
-                        <h1 class="pt-3" style="font-size: 16px">THAO TÁC CHỨC NĂNG</h1>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="bg-white rounded shadow p-3 mb-4 mt-4">
+                <form action="">
+                    <div class="row">
+                        <div class="mb-3">
+                            <label class="form-label">MÃ KHEN THƯỞNG</label>
+                            <input type="text" class="form-control" disabled>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">TÊN KHEN THƯỞNG</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label mb-2 font-weight-bold">Mô tả</label>
+                            <textarea name="description" class="form-control" id="description" rows="50" cols="50"></textarea>
+                        </div>
                     </div>
-                    <div>
-                        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                            <div class="card">
-                                <div class="card-header p-3 pt-2">
-                                    <div
-                                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                                        <i class="material-icons opacity-10">weekend</i>
-                                    </div>
-                                    <div class="text-end pt-1">
-                                        <p class="text-sm mb-0 text-capitalize">Today's Money</p>
-                                        <h4 class="mb-0">$53k</h4>
-                                    </div>
-                                </div>
-                                <hr class="dark horizontal my-0">
-                                <div class="card-footer p-3">
-                                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
-                                        last week</p>
-                                </div>
-                            </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-success">THÊM MỚI</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="bg-white rounded shadow p-3 mb-4 mt-4">
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <h5>DANH SÁCH LOẠI KHEN THƯỞNG</h5>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <div class="input-group w-50">
+                            <span class="input-group-text">
+                                <svg class="icon icon-xs" x-description="Heroicon name: solid/search"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                            <input type="text" class="form-control" placeholder="Search">
                         </div>
                     </div>
                 </div>
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">STT</th>
+                            <th scope="col">MÃ KHEN THƯỞNG</th>
+                            <th scope="col">TÊN KHEN THƯỞNG</th>
+                            <th scope="col">MÔ TẢ</th>
+                            <th scope="col">NGÀY TẠO</th>
+                            <th scope="col">NGÀY SỬA</th>
+                            <th scope="col">THAO TÁC</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>KH12345</td>
+                            <td>Đạt doanh số</td>
+                            <td>Khen thưởng KPI tháng 9</td>
+                            <td>2022-02-02</td>
+                            <td>2023-03-02</td>
+                            <td class="text-center">
+                                <a href="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
+                                        viewBox="0 0 512 512">
+                                        <path
+                                            d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                                    </svg>
+                                </a>
+                                <a href="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14"
+                                        viewBox="0 0 448 512">
+                                        <path
+                                            d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- routes/admin/manage_reward.php
+++ routes/admin/manage_reward.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('/reward')->name('reward.')->group(function () {
-    // Trang chủ
-    // Route::get('/', [PositionController::class, 'index'])->name('home');
+    // Hiển thị view khen thưởng
 
-    // Danh sách + thêm mới chức vụ
     Route::get('/', function () {
         return view("admin.pages.reward.ManageReward");
     })->name('index');
