29fd3025df3d7035bbee45a9972f36c0059a4917
TruongVanPhuoc <phuoc250320@example.com>
2024-01-15 15:23:59 +0700

    format code

--- app/Http/Controllers/Employee/User/UserController.php
+++ app/Http/Controllers/Employee/User/UserController.php
@@ -13,7 +13,6 @@ class UserController extends Controller
     public function __construct(EmployeeService $employeeService)
     {
         $this->employeeService = $employeeService;
-
     }
     public function getEmployeeInfor()
     {
