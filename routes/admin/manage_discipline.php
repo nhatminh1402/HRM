a4af4f12f528f728eca4ba39c5291c94ed2a0709
Phước Nguyễn <phuocnguyen.081096@gmail.com>
2024-01-15 13:53:22 +0700

    create_layouts_dsictipline

--- routes/admin/manage_discipline.php
+++ routes/admin/manage_discipline.php
@@ -9,23 +9,4 @@ Route::prefix('/discipline')->name('discipline.')->group(function () {
     Route::get('/', function () {
         return view('admin.pages.discipline.index');
     })->name('index');
-
-    // //Thêm mới chức vụ
-    // Route::post('/', [PositionController::class, 'store'])->name('positions.store');
-
-    // // Sửa chức vụ
-    // Route::get('/edit-position/{id}', [PositionController::class, 'edit'])->name('edit-position');
-
-    // Route::put('/update/{id}', [PositionController::class, 'update'])->name('positions.update');
-
-    // // Danh sách + thêm mới chuyên ngành
-    // Route::get('/major', function () {
-    //     return view('admin.pages.employee_management.major');
-    // })->name('major');
-
-    // // Tìm kiêm danh sách chức vụ
-    // Route::get('/search', [PositionController::class, 'index'])->name('search-position');
-
-    // // Xóa chức vụ
-    // Route::delete('/position/{id}', [PositionController::class, 'destroy'])->name('position.destroy');
 });
