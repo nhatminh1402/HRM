9025f45f5cf6dc8a06e1382e5b3457185c9083cf
Phước Nguyễn <phuocnguyen.081096@gmail.com>
2024-01-17 11:33:32 +0700

    create layouts manager projects

--- routes/admin/manage_project.php
+++ routes/admin/manage_project.php
@@ -14,25 +14,4 @@ Route::prefix('/project')->name('project.')->group(function () {
      Route::get('/edit', function(){
         return view('admin.pages.project.edit_project');
     })->name('edit');
-
-    // //Thêm mới chức vụ
-    // Route::post('/', [PositionController::class, 'store'])->name('positions.store');
-
-    // // Sửa chức vụ
-    // Route::get('/edit-position/{id}', [PositionController::class, 'edit'])->name('edit-position');
-
-    // Route::put('/update/{id}', [PositionController::class, 'update'])->name('positions.update');
-
-    // // Danh sách + thêm mới chuyên ngành
-    // Route::get('/major', function () {
-    //     return view('admin.pages.employee_management.major');
-    // })->name('major');
-
-    // // Tìm kiêm danh sách chức vụ
-    // Route::get('/search', [PositionController::class, 'index'])->name('search-position');
-
-    // // Xóa chức vụ
-    // Route::delete('/position/{id}', [PositionController::class, 'destroy'])->name('position.destroy');
-
-
 });
