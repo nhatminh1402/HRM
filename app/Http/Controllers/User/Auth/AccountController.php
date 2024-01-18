12917993fdc717cdfd1efca4e6c881b5b8e30fc4
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-18 09:59:15 +0700

    Done change password for user

--- app/Http/Controllers/User/Auth/AccountController.php
+++ app/Http/Controllers/User/Auth/AccountController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\ManageAccount;
+namespace App\Http\Controllers\User\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ChangePasswordRequest;
@@ -8,22 +8,23 @@ use Auth;
 use Hash;
 use Illuminate\Http\Request;
 
-class PasswordController extends Controller
+class AccountController extends Controller
 {
-    public function ChangePassword()
+    public function showViewChangePassword()
     {
-        return view('admin.pages.user.change_password');
+        return view('user.pages.change_password');
     }
 
-    public function saveChangePassword(ChangePasswordRequest $request)
+    public function changePassword(ChangePasswordRequest $request)
     {
-        $user = Auth::user();
+        $user = Auth::guard('employee')->user();
+
         if (!Hash::check($request->current_password, $user->password)) {
             return back()->withErrors(['current_password' => 'Mật khẩu hiện tại không chính xác.']);
         }
 
         $user->update(['password' => $request->new_password]);
         toastr()->success('Cập nhật mật khẩu thành công!');
-        return redirect()->route('user.change-password');
+        return redirect()->route('user.view-change-password');
     }
 }
--- routes/user/user.php
+++ routes/user/user.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\Auth\AccountController;
 use App\Http\Controllers\User\User\UserController;
 use Illuminate\Support\Facades\Route;
 /*
@@ -14,54 +15,55 @@ use Illuminate\Support\Facades\Route;
 */
 
 // 1. Router cho trang Home
-Route::get("home-page", function () {
-    return view("user.pages.index");
+Route::get('home-page', function () {
+    return view('user.pages.index');
 })->name('home');
 
 // 2. Router cho trang xem thông tin chi tiết nhân viên
-Route::get("employee-infor",[UserController::class,'getEmployeeInfor'])->name('employee-info');
+Route::get('employee-infor', [UserController::class, 'getEmployeeInfor'])->name('employee-info');
 
 // 3. Router cho trang xem các phòng ban
-Route::get("departments", function () {
-    return view("user.pages.manage_department");
+Route::get('departments', function () {
+    return view('user.pages.manage_department');
 })->name('departments');
 
 // 4. Router cho trang xem danh sách nhân viên của phòng ban
-Route::get("department-detail", function () {
-    return view("user.pages.department_detail");
+Route::get('department-detail', function () {
+    return view('user.pages.department_detail');
 })->name('department-detail');
 
 // 5. Router cho trang xem bảng lương cá nhân
-Route::get("salary", function () {
-    return view("user.pages.manage_salary");
+Route::get('salary', function () {
+    return view('user.pages.manage_salary');
 })->name('salary');
 
 // 6. Router cho trang xem danh sách công tác
-Route::get("schedule", function () {
-    return view("user.pages.manage_business_travel");
+Route::get('schedule', function () {
+    return view('user.pages.manage_business_travel');
 })->name('schedule');
 
 //7. Router cho trang xem danh sách khen thưởng
-Route::get("reward", function () {
-    return view("user.pages.list_reward");
-})->name("reward");
+Route::get('reward', function () {
+    return view('user.pages.list_reward');
+})->name('reward');
 
 //8. Router cho trang xem lịch sử bị kỷ luật
-Route::get("punishments", function () {
-    return view("user.pages.list_punishment");
-})->name("punishments");
+Route::get('punishments', function () {
+    return view('user.pages.list_punishment');
+})->name('punishments');
 
 //9. Router cho trang xem thông tin tài khoản
-Route::get("infors", function () {
-    return view("user.pages.user_infors");
-})->name("infors");
+Route::get('infors', function () {
+    return view('user.pages.user_infors');
+})->name('infors');
 
-//10. Router cho trang đổi mật khẩu
-Route::get("change-password", function () {
-    return view("user.pages.change_password");
-})->name("change-password");
+//10. Router cho view đổi mật khẩu
+Route::get('change-password', [AccountController::class, 'showViewChangePassword'])->name('view-change-password');
 
-//11. Router cho trang hien thi timesheet
-Route::get("timesheet-user", function () {
-    return view("user.pages.time_sheet");
-})->name("timesheet-user");
+//11. Router đổi mật khẩu
+Route::post('change-password', [AccountController::class, 'changePassword'])->name('change-password');
+
+//12. Router cho trang hiển thị timesheet
+Route::get('timesheet-user', function () {
+    return view('user.pages.time_sheet');
+})->name('timesheet-user');
