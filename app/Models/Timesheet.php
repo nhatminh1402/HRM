cc1e76f0834cbb731117101f7d6e1b675d3af30a
TruongVanPhuoc <phuoc250320@example.com>
2024-01-18 14:36:33 +0700

    formatcode

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Timesheet extends Model
-{
-    
+{ 
     protected $table = 'timesheet';
     protected $fillable = [
         'employee_id'
