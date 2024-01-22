42eb6864e4647311aeeae3cd79276cac2633901e
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-22 14:35:31 +0700

    Update create function using ajax

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- start head are -->
-@include('admin.layouts.head')
-<!-- end head are -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <meta name="theme-color" content="#ffffff">
+    <!-- Main styles for this application-->
+    <link href="{{ asset('CoreUI/css/style.css') }}" rel="stylesheet">
+    {{-- Jquery --}}
+    <script src="{{ asset('lib/jquery-3.7.1.min') }}"></script>
+    {{-- sweetalert --}}
+    <script src="{{ asset('assets/vendor/sweetalert2/dist/sweetalert2.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/sweetalert2/dist/sweetalert2.min.css') }}">
+
+    <script src="{{ asset('assets/vendor/@popperjs/core/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    @yield('css')
+</head>
 
 <body>
     <!-- start main side bar -->
@@ -18,11 +34,18 @@
                 @yield('content')
             </div>
         </div>
-        @include('admin.layouts.footer')
+        <footer class="footer">
+            <div>© HỆ THỐNG QUẢN LÝ NHÂN SỰ</div>
+        </footer>
     </div>
+    <!-- CoreUI and necessary plugins-->
+    <script src="{{ asset('CoreUI/vendors/@coreui/coreui/js/coreui.bundle.min.js') }}"></script>
+    <script src="{{ asset('CoreUI/vendors/simplebar/js/simplebar.min.js') }}"></script>
+    <!-- Plugins and scripts required by this view-->
+    <script src="{{ asset('CoreUI/vendors/@coreui/utils/js/coreui-utils.js') }}"></script>
+    <script src="{{ asset('CoreUI/js/main.js') }}"></script>
 
     @yield('script')
-
 </body>
 
 </html>
