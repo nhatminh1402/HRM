fa6b40ad4eaf71e244fda4d654e47e0f87915852
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-22 09:09:28 +0700

    Change template Admin

---
+++ resources/views/admin/layouts/head.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>CoreUI Free Bootstrap Admin Template</title>
+    <meta name="theme-color" content="#ffffff">
+    <!-- Vendors styles-->
+    <link rel="stylesheet" href="{{ asset('CoreUI/vendors/simplebar/css/simplebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('CoreUI/css/vendors/simplebar.css') }}">
+    <!-- Main styles for this application-->
+    <link href="{{ asset('CoreUI/css/style.css') }}" rel="stylesheet">
+    {{-- Jquery --}}
+    <script src="{{ asset('lib/jquery-3.7.1.min') }}"></script>
+    {{-- sweetalert --}}
+    <script src="{{ asset('assets/vendor/sweetalert2/dist/sweetalert2.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/vendor/sweetalert2/dist/sweetalert2.min.css') }}">
+
+    @yield('css')
+</head>
