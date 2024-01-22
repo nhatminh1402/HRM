47918730745e3d8c5781054082332450187ef501
nhatminh1402 <nhatminhle1402@gmail.com>
2024-01-22 09:21:04 +0700

    Done template admin

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>CoreUI Free Bootstrap Admin Template</title>
+    <title>@yield('title')</title>
     <meta name="theme-color" content="#ffffff">
     <!-- Vendors styles-->
     <link rel="stylesheet" href="{{ asset('CoreUI/vendors/simplebar/css/simplebar.css') }}">
