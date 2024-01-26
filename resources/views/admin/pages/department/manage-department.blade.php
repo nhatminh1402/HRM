95cb8ac00e0250edb30828e161ed9d4b2f46bafd
TruongVanPhuoc <phuoc250320@example.com>
2024-01-26 10:35:07 +0700

    fix template

--- resources/views/admin/pages/department/manage-department.blade.php
+++ resources/views/admin/pages/department/manage-department.blade.php
@@ -59,38 +59,43 @@
             <h2 class="mb-4 pb-2  text-primary">Nhân viên trong phòng ban</h2>
             <div class="card">
                 <div class="table-responsive">
+                    <table class="table table-bordered table-centered ">
+                        <thead class="border">
+                            <tr class="table border">
+                                <th class="rounded-start text-center">STT</th>
+                                <th class="text-center">Mã nhân viên</th>
+                                <th style="width: 150px;" class="text-center">Ảnh</th>
+                                <th class="text-center">Tên nhân viên</th>
+                                <th class="text-center">Giới tính</th>
+                                <th class="text-center">Ngày sinh</th>
+                                <th class="text-center">Chức vụ</th>
+                                <th class="text-center">Ngày thêm</th>
+                                <th class="text-center rounded-end">Tình trạng</th>
+                                <th class="text-center rounded-end">Action</th>
 
-                    <table class="table table-centered table-nowrap mb-0 rounded">
-                        <tr>
-                            <th class="border-0 rounded-start text-center">STT</th>
-                            <th class="border-0  text-center">Mã nhân viên</th>
-                            <th class="border-0  text-center">Ảnh</th>
-                            <th class="border-0  text-center">Tên nhân viên</th>
-                            <th class="border-0  text-center">Giới tính</th>
-                            <th class="border-0  text-center">Năm sinh</th>
-                            <th class="border-0  text-center">Chức vụ</th>
-                            <th class="border-0  text-center">Ngày thêm</th>
-                            <th class="border-0  text-center">Trạng thái</th>
-                            <th class="border-0  text-center rounded-end">Action</th>
-                        </tr>
+
+                            </tr>
                         </thead>
                         <tbody>
-                            @if ($employees->isEmpty())
-                                <td colspan="9" class="text-lg-center w-100">Không thấy nhân viên nào trong phòng ban này
-                                    !</td>
-                            @else
+                            @if (!empty($employees))
                                 @foreach ($employees as $key => $employee)
                                     <tr>
                                         <td class='text-center'>{{ $key + 1 }}</td>
                                         <td class='text-center'>{{ $employee->code_employee }}</td>
                                         <td class='text-center'>
-                                            <img class="image image-md" alt="Image placeholder"
-                                                src="/uploads/{{ $employee->image }}">
+                                            <img style="width: 80%; height: auto" class="image image-md"
+                                                alt="Image placeholder" src="/uploads/{{ $employee->image }}">
                                         </td>
                                         <td class='text-center'>{{ $employee->full_name }}</td>
-                                        <td class='text-center'>{{ $employee->gender ? 'Name' : 'Nữ' }}</td>
+                                        <td class='text-center'>
+                                            @if ($employee->gender == 1)
+                                                Nam
+                                            @else
+                                                Nữ
+                                            @endif
+                                        </td>
                                         <td class='text-center'>{{ $employee->dob }}</td>
-                                        <td class='text-center'>{{ $employee->department->name ?? '' }}</td>
+                                        <td class='text-center'>{{ $employee->position->name ?? '' }}</td>
                                         <td class='text-center'>{{ $employee->created_at->format('Y-m-d') }}</td>
                                         <td class='text-center'>{{ $employee->status ? 'Đang làm viêc' : 'Đã nghĩ việc' }}
                                         </td>
@@ -101,17 +106,20 @@
                                                     height="16" width="14" viewBox="0 0 448 512">
                                                     <path
                                                         d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3
-                                                                                                                                                                                                                                                                    32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2
-                                                                                                                                                                                                                                                                    6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
+                                                                                                                                                                                                                                                                        32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2
+                                                                                                                                                                                                                                                                        6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"
                                                         fill="#fff" />
-                                            </svg></a>
+                                                </svg></a>
                                         </td>
+
                                     </tr>
                                 @endforeach
+                            @else
+                                <td colspan="9" class="text-lg-center w-100">Không có nhân viên nào trong phòng ban !</td>
                             @endif
-
                         </tbody>
                     </table>
+                    {{ $employees->links() }}
                 </div>
             </div>
         </div>
